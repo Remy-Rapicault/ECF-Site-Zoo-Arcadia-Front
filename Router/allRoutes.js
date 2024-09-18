c94ff43cfffc0fc21536f7fe79feacebbcece485
Remy-Rapicault <remy.rapicault.dwwm@gmail.com>
2024-09-18 17:23:46 +0200

    Add pages signin

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,8 +4,10 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/services", "Les Services", "/pages/services.html"),
-    new Route("/habitats", "Les Habitats", "../pages/habitats.html"),
-    new Route("/savane", "La Savane", "../pages/savane.html"),
+    new Route("/habitats", "Les Habitats", "/pages/habitats.html"),
+    new Route("/savane", "La Savane", "/pages/savane.html"),
+    new Route("/signin", "Connexion", "/pages/signin.html"),
+    new Route("/signup", "inscription", "/pages/signup.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
